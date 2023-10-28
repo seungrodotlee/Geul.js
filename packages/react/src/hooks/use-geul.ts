379ae107e9866ef6c041f3ea510e4fc2425bc642
seungrodotlee <seungrodotlee@gmail.com>
2023-10-28 23:15:14 +0900

    fix. Fix 'useGeul' hook
    <description>
    - Throw error when initial value is not included in destiny value
    - Start type writing from initial value

--- packages/react/src/hooks/use-geul.ts
+++ packages/react/src/hooks/use-geul.ts
@@ -1,18 +1,20 @@
-import { phonemes } from "./../constraints/phonemes";
-import { useEffect, useMemo, useState } from "react";
-import { delay, interval, map, of, reduce, scan, take } from "rxjs";
-import { phonemesSeperator } from "../utils/phonemes/phonemes.seperator";
-import { phonemesMerger } from "../utils/phonemes/phonemes.merger";
-import { pipe, slice, toArray } from "@fxts/core";
-import { match } from "ts-pattern";
+import { useMemo, useState } from "react";
 
-type UseGeulOptions = {
+import { interval, map, take } from "rxjs";
+import { pipe, slice, toArray } from "@fxts/core";
+import { P, match } from "ts-pattern";
+
+import { phonemesMerger } from "../utils/phonemes/phonemes.merger";
+import { phonemesSeperator } from "../utils/phonemes/phonemes.seperator";
+import { partOf } from "../utils/list/part-of";
+
+export type UseGeulOptions = {
   speed: number;
   initial?: string;
 };
 export const useGeul = (
   value: string,
-  { speed, initial = "" }: UseGeulOptions
+  { speed, initial = "" }: UseGeulOptions,
 ) => {
   const phonemes = useMemo(() => phonemesSeperator(value), [value]);
   const [geul, setGeul] = useState<string>(initial);
@@ -32,17 +34,34 @@ export const useGeul = (
             If you want to re-run it. call 'reset' method first.
           `
             .replace(/^ +/gm, "")
-            .trim()
-        )
+            .trim(),
+        ),
       )
       .otherwise(() => {
-        setFired(true);
-        interval(speed)
-          .pipe(
-            map((idx) => pipe(phonemes, slice(0, idx), toArray)),
-            take(phonemes.length + 1)
+        match(phonemesSeperator(initial))
+          .with(
+            P.when((initPhonemes) =>
+              partOf(phonemesSeperator(value), initPhonemes),
+            ),
+            (initPhonemes) => {
+              setFired(true);
+              interval(speed)
+                .pipe(
+                  map((idx) =>
+                    pipe(
+                      phonemes,
+                      slice(0, initPhonemes.length + idx),
+                      toArray,
+                    ),
+                  ),
+                  take(phonemes.length - initPhonemes.length + 1),
+                )
+                .subscribe((value) => setGeul(phonemesMerger(value)));
+            },
           )
-          .subscribe((value) => setGeul(phonemesMerger(value)));
+          .otherwise(() => {
+            throw Error(`Initial value '${initial}' is not part of '${value}'`);
+          });
       });
   };
 
