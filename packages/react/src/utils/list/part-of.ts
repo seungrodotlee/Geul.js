1a1bc43bb516ea7a600f563361a701f9d5c6abe0
seungrodotlee <seungrodotlee@gmail.com>
2023-10-28 23:10:28 +0900

    feat. Work with 'partOf' function
    <description>
    - Implement compare two lists('A', 'B') and 'B' is included in 'A'

---
+++ packages/react/src/utils/list/part-of.ts
@@ -0,0 +1,26 @@
+import { join, map, pipe } from "@fxts/core";
+import { P, match } from "ts-pattern";
+
+export const partOf = <T>(list: T[], comparisonTarget?: T[]) => {
+  return match(comparisonTarget)
+    .with(
+      P.nullish,
+      () => (comparisonTarget: T[]) => partOf(list, comparisonTarget),
+    )
+    .with(
+      P.when((target) => typeof target[0] === "object"),
+      (target) =>
+        pipe(
+          list,
+          map((item) => JSON.stringify(item)),
+          join(""),
+        ).includes(
+          pipe(
+            target as T[],
+            map((item) => JSON.stringify(item)),
+            join(""),
+          ),
+        ),
+    )
+    .otherwise((target) => join("", list).includes(join("", target)));
+};
