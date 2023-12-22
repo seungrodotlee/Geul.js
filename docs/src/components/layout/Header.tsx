39704f085faaceb30d4aba05793d866e3653f2dc
이승로 <iseungro@iseungloui-MacBookPro.local>
2023-12-22 21:52:46 +0900

    feat. Add index.ts to pack. react
    <description>
    - to export funtions

--- docs/src/components/layout/Header.tsx
+++ docs/src/components/layout/Header.tsx
@@ -1,9 +1,15 @@
+import { useDynamicGeul, useGeul } from "@geul/react";
+
 const Header = () => {
+  const { geul, run } = useDynamicGeul("", {
+    speed: 100,
+  });
   return (
     <div>
-      Header!
+      <p className="absolute">글.제이에스</p>
+      <p className="relative"></p>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
--- docs/src/components/layout/Layout.tsx
+++ docs/src/components/layout/Layout.tsx
@@ -4,9 +4,9 @@ import SideBar from "./SideBar";
 
 const Layout = ({ children, className, ...props }: ComponentProps<"div">) => {
   return (
-    <div className={classNames("flex w-full min-h-full", className)} {...props}>
+    <div className={classNames("flex w-full h-full", className)} {...props}>
       <SideBar />
-      <div className="flex-grow">{children}</div>
+      <div className="flex-grow overflow-y-auto">{children}</div>
     </div>
   );
 };
